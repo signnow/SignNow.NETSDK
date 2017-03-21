8c6517317a58f27ea939c717e786648c97ad4b5c
Michael Fitchett <mfitchett@barracuda.com>
2017-03-21 11:24:59 -0700

    Added new methods to user

--- CSLibrary/User.cs
+++ CSLibrary/User.cs
@@ -33,8 +33,54 @@ namespace CudaSign
                 .AddHeader("Accept", "application/json")
                 .AddHeader("Authorization", "Basic " + Config.EncodedClientCredentials);
 
-                request.RequestFormat = DataFormat.Json;
-                request.AddBody(new { email = Email, password = Password, first_name = FirstName, last_name = LastName });
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new { email = Email, password = Password, first_name = FirstName, last_name = LastName });
+
+            var response = client.Execute(request);
+
+            dynamic results = "";
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                results = response.Content;
+            }
+            else
+            {
+                Console.WriteLine(response.Content.ToString());
+                results = response.Content.ToString();
+            }
+
+            if (ResultFormat == "JSON")
+            {
+                results = JsonConvert.DeserializeObject(results);
+            }
+            else if (ResultFormat == "XML")
+            {
+                results = (XmlDocument)JsonConvert.DeserializeXmlNode(results, "root");
+            }
+
+            return results;
+        }
+
+        /// <summary>
+        /// Updates a User Account
+        /// </summary>
+        /// <param name="AccessToken">User's Access Token</param>
+        /// <param name="FirstName">New User's First Name</param>
+        /// <param name="LastName">New User's Last Name</param>
+        /// <param name="ResultFormat">JSON, XML</param>
+        /// <returns>Success Message</returns>
+        public static dynamic Update(string AccessToken, string FirstName = "", string LastName = "", string ResultFormat = "JSON")
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri(Config.ApiHost);
+
+            var request = new RestRequest("/user", Method.PUT)
+                .AddHeader("Accept", "application/json")
+                .AddHeader("Authorization", "Bearer " + AccessToken);
+
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new { first_name = FirstName, last_name = LastName });
 
             var response = client.Execute(request);
 
@@ -67,9 +113,8 @@ namespace CudaSign
         /// </summary>
         /// <param name="AccessToken">User's Access Token</param>
         /// <param name="ResultFormat">JSON, XML</param>
-        /// <param name="ResultFormat">JSON, XML</param>
         /// <returns>User Account Information</returns>
-        public static dynamic Get(string AccessToken,  string ResultFormat = "JSON")
+        public static dynamic Get(string AccessToken, string ResultFormat = "JSON")
         {
             var client = new RestClient();
             client.BaseUrl = new Uri(Config.ApiHost);
@@ -77,6 +122,50 @@ namespace CudaSign
             var request = new RestRequest("/user", Method.GET)
                 .AddHeader("Accept", "application/json")
                 .AddHeader("Authorization", "Bearer " + AccessToken);
+
+            var response = client.Execute(request);
+
+            dynamic results = "";
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                results = response.Content;
+            }
+            else
+            {
+                Console.WriteLine(response.Content.ToString());
+                results = response.Content.ToString();
+            }
+
+            if (ResultFormat == "JSON")
+            {
+                results = JsonConvert.DeserializeObject(results);
+            }
+            else if (ResultFormat == "XML")
+            {
+                results = (XmlDocument)JsonConvert.DeserializeXmlNode(results, "root");
+            }
+
+            return results;
+        }
+
+        /// <summary>
+        /// Turns off Reusable Signature
+        /// </summary>
+        /// <param name="AccessToken">User's Access Token</param>
+        /// <param name="ResultFormat">JSON, XML</param>
+        /// <returns>Success Message</returns>
+        public static dynamic TurnOffReusableSignatures(string AccessToken, string ResultFormat = "JSON")
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri(Config.ApiHost);
+
+            var request = new RestRequest("/user/setting/no_user_signature_return", Method.PUT)
+                .AddHeader("Accept", "application/json")
+                .AddHeader("Authorization", "Bearer " + AccessToken);
+
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new { active = 1 });
 
             var response = client.Execute(request);
 
